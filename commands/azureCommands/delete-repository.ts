b0afbee33705b78f395b6f1edc2fff0a4bc24a39
Esteban Rey <erey@stanford.edu>
2018-08-09 12:25:15 -0700

    Cleaned code

--- commands/azureCommands/delete-repository.ts
+++ commands/azureCommands/delete-repository.ts
@@ -1,11 +1,11 @@
 import { Registry } from "azure-arm-containerregistry/lib/models";
 import { SubscriptionModels } from 'azure-arm-resource';
-import request = require('request-promise');
 import * as vscode from "vscode";
 import * as quickPicks from '../../commands/utils/quick-pick-azure';
 import { AzureRepositoryNode } from '../../explorer/models/AzureRegistryNodes';
 import * as acrTools from '../../utils/Azure/acrTools';
 import { Repository } from "../../utils/Azure/models/repository";
+
 const teleCmdId: string = 'vscode-docker.deleteACRRepository';
 /**
  * function to delete an Azure repository and its associated images
@@ -16,38 +16,34 @@ export async function deleteRepository(context?: AzureRepositoryNode): Promise<v
     let registry: Registry;
     let subscription: SubscriptionModels.Subscription;
     let repoName: string;
-    let username: string;
-    let password: string;
-    if (!context) {
+
+    if (context) {
+        repoName = context.label;
+        subscription = context.subscription;
+        registry = context.registry;
+    } else {
         registry = await quickPicks.quickPickACRRegistry();
         subscription = acrTools.getRegistrySubscription(registry);
-        let repository: Repository = await quickPicks.quickPickACRRepository(registry);
+        const repository: Repository = await quickPicks.quickPickACRRepository(registry);
         repoName = repository.name;
     }
 
-    // ensure user truly wants to delete registry
+    // Ensure user truly wants to delete registry
     let opt: vscode.InputBoxOptions = {
         ignoreFocusOut: true,
         placeHolder: 'No',
         value: 'No',
         prompt: 'Are you sure you want to delete this repository and its associated images? Enter Yes to continue: '
     };
+
     let answer = await vscode.window.showInputBox(opt);
     answer = answer.toLowerCase();
     if (answer !== 'yes') { return; }
 
-    // generate credentials before requesting a delete.
-    if (context) {
-        username = context.userName;
-        password = context.password;
-        repoName = context.label;
-        subscription = context.subscription;
-        registry = context.registry;
-    } else { // this is separated from !context above so it only calls loginCredentials once user has assured they want to delete the repository
-        let creds = await acrTools.loginCredentials(subscription, registry);
-        username = creds.username;
-        password = creds.password;
-    }
+    let creds = await acrTools.loginCredentials(subscription, registry);
+    const username: string = creds.username;
+    const password: string = creds.password;
     let path = `/v2/_acr/${repoName}/repository`;
     await acrTools.sendRequestToRegistry('delete', registry.loginServer, path, username, password);
+
 }
